c2b0e908385983a6e7d1512190a9a1b140b66a16
RicHard <richardicode@gmail.com>
2024-03-25 15:49:58 +0800

    add git tag version

--- tag_git_v1/main.go
+++ tag_git_v1/main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("learning how to use git tag to control version")
+	/*
+		Given a version number MAJOR.MINOR.PATCH, increment the:
+		■ MAJOR changes, not backwards compatible
+		■ MINOR changes, backwards compatible
+		■ PATCH big fixes, backwards compatible
+	*/
 }
